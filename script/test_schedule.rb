94e701d8a442b5ad94edb19341941a8e42183c81
Will Langstroth <will@langstroth.com>
2013-04-13 22:27:31 -0400

    comment out some of the script test

--- script/test_schedule.rb
+++ script/test_schedule.rb
@@ -5,6 +5,7 @@ require 'active_support/all'
 require './lib/rrschedule.rb'
 include RRSchedule
 
+=begin
 schedule1 = RRSchedule::Schedule.new(
               :teams => %w(T1 T2 T3 T4 T5 T6 T7 T8 T9 T10 T11 T12 T13 T14 T15 T16 T17 T18 T19 T20 T21 T22 T23 T24 T25 T26),
               :rules => [
@@ -14,28 +15,28 @@ schedule1 = RRSchedule::Schedule.new(
                   :ps => ["1","2","3","4","5","6"],
                 )
               ],
-              :shuffle => true,
               :start_date => Date.parse("2010/10/13")
             ).generate
 
 puts schedule1.to_s
+=end
 
+# Default start date (today) and default
 schedule2 = RRSchedule::Schedule.new(
-              :teams => %w(T1 T2 T3 T4 T5 T6 T7 T8 T9 T10 T11 T12 T13),
+              :teams => %w(T1 T2 T3 T4 T5 T6 T7 T8 T9 T10 T11 T12 T13 T14 T15 T16 T17 T18 T19 T20),
               :rules => [
                 RRSchedule::Rule.new(
                   :wday => 3,
-                  :gt => ["7:00 PM"],
-                  :ps => ["1","2","3","4"],
+                  :game_times => ["7:00 PM","8:00 PM","9:00 PM"],
+                  :playing_surfaces => ["1","2","3","4","5","6","7","8"],
                 )
               ],
-              :cycles => 3,
-              :shuffle => true,
-              :start_date => Date.parse("2010/10/13")
+              :cycles => 2
             ).generate
 
 puts schedule2.to_s
 
+=begin
 schedule3 = RRSchedule::Schedule.new(
               :teams => [
                 %w(A1 A2 A3 A4 A5 A6 A7 A8 A9),
@@ -67,3 +68,4 @@ schedule3 = RRSchedule::Schedule.new(
             ).generate
 
 puts schedule3.to_s
+=end
